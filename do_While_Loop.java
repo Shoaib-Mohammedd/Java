7913df7260e1dec17d8781318f2905b8b039b78a
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-09 10:45:38 +0300

    do_While_Loop.java

---
+++ do_While_Loop.java
@@ -0,0 +1,10 @@
+public class do_While_Loop {
+    public static void main(String[] args) {
+        int num = 5;
+        do {
+            System.out.println( num);
+            num++;
+        }while ( num <= 5);
+        System.out.println( " Hello World ");
+    }
+}
