6534e2618987cd4eeb45528897225da0e294c291
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-09 10:39:53 +0300

    Forloobs Ex

---
+++ ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int counter = 1; counter!=5; counter++);
+        System.out.println(" For Loop is amazing !");
+    }
+
+}
