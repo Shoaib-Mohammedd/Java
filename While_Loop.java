a53dde4e61a8e7bbb0920556aaad815edc874338
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-09 10:44:57 +0300

    While Loop

---
+++ While_Loop.java
@@ -0,0 +1,9 @@
+public class While_Loop {
+    public static void main(String[] args) {
+        int num = 1;
+        while ( num <= 5 ){
+            System.out.println( num);
+            num += num;
+        }
+    }
+}
